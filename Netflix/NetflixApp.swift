d62120b42bfa4bb947fce038c5e8f7863d1fd868
Jean Paull <t2103jnp@prestadorcbmp.com.br>
2021-08-17 19:42:17 -0300

    Initial Commit

---
+++ Netflix/NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixApp.swift
+//  Netflix
+//
+//  Created by Jean Paul Borges Manzini on 17/08/21.
+//
+
+import SwiftUI
+
+@main
+struct NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
